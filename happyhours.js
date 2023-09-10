703b1616fb1e031f641c33fa265cf6089573c684
el7 <got2skatetoday@gmail.com>
2023-09-10 02:02:35 -0700

    breaking out into functions

--- happyhours.js
+++ happyhours.js
@@ -1,7 +1,6 @@
 /* import deal info */
 import data from './happyhours.json' assert { type: 'json' };
 
-
 /* create variables */
 var dealActive = false;
 var targetDiv = document.getElementById('container-hh-result');
@@ -19,6 +18,39 @@ for (var i = 0; i < data.Restaurants.length; i++) {
 
 	targetDiv.appendChild(contentRestaurantName);
 
+	addToDisplayBizHours();
+
+	addToDisplaySpecials();
+}
+
+function addToDisplayDeals () {
+
+	var contentSpecialName = document.createTextNode("\n> " + sp.Name + "\n");
+	targetDiv.appendChild(contentSpecialName);
+
+	/* iterate over deals for the special */
+	for (var l = 0; l < sp.Details.length; l++) {
+
+		var de = sp.Details[l];
+		var contentDealName = document.createTextNode("- " + de.Name + ": ");
+		var contentDealValue = document.createTextNode(de.DealValue + " ");
+		var contentDealModifier = document.createTextNode(de.DealModifier);
+		var contentDealNote = document.createTextNode("(" + de.DealNote + ")\n");
+
+		targetDiv.appendChild(contentDealModifier);
+		targetDiv.appendChild(contentDealValue);
+		targetDiv.appendChild(contentDealName);
+		targetDiv.appendChild(contentDealNote);
+
+	}
+
+
+
+}
+
+
+function addToDisplayBizHours () {
+
 	/* iterate over biz hours info */
 	for (var iter_hours = 0; iter_hours < re.Hours.length; iter_hours++) {
 	
@@ -35,56 +67,40 @@ for (var i = 0; i < data.Restaurants.length; i++) {
 		}
 	}
 
-	/* iterate over specials for the biz */
-	for (var j = 0; j < re.Specials.length; j++) {
-
-		var sp = re.Specials[j];
-
-		/* iterate over days/hours the special is active */
-		for (var k = 0; k < sp.Days.length; k++) {
-			var da = sp.Days[k];
-
-			if (da.StartTime == "open") da.StartTime = openTime;
-			if (da.EndTime == "close") da.EndTime = closeTime;
-
-			var datetimeDealStart = new Date(da.StartTime);
-			var datetimeDealEnd = new Date(da.EndTime);
-
-			/* check if this deal is active // WORKS */
-			if (datetimeNow.getHours() >= datetimeDealStart.getHours() && datetimeNow.getHours() <= datetimeDealEnd.getHours()) {
-
-				dealActive = true;
-
-			} else {
-
-				dealActive = false;
-			}
-		}
-
-		/* if deal active, add to div */
-		if (dealActive == true) {
-
-			var contentSpecialName = document.createTextNode("\n> " + sp.Name + "\n");
-			targetDiv.appendChild(contentSpecialName);
-
-			for (var l = 0; l < sp.Details.length; l++) {
-
-				var de = sp.Details[l];
-				var contentDealName = document.createTextNode("- " + de.Name + ": ");
-				var contentDealValue = document.createTextNode(de.DealValue + " ");
-				var contentDealModifier = document.createTextNode(de.DealModifier);
-				var contentDealNote = document.createTextNode("(" + de.DealNote + ")\n");
-
-				targetDiv.appendChild(contentDealModifier);
-				targetDiv.appendChild(contentDealValue);
-				targetDiv.appendChild(contentDealName);
-				targetDiv.appendChild(contentDealNote);
-
-			}
-		}
-	}
 }
 
+function addToDisplaySpecials(){
+		/* iterate over specials for the biz */
+		for (var j = 0; j < re.Specials.length; j++) {
 
-
-
+			var sp = re.Specials[j];
+	
+			/* iterate over days/hours the special is active */
+			for (var k = 0; k < sp.Days.length; k++) {
+				var da = sp.Days[k];
+	
+				if (da.StartTime == "open") da.StartTime = openTime;
+				if (da.EndTime == "close") da.EndTime = closeTime;
+	
+				var datetimeDealStart = new Date(da.StartTime);
+				var datetimeDealEnd = new Date(da.EndTime);
+	
+				/* check if this deal is active // WORKS */
+				if (datetimeNow.getHours() >= datetimeDealStart.getHours() && datetimeNow.getHours() <= datetimeDealEnd.getHours()) {
+	
+					dealActive = true;
+	
+				} else {
+	
+					dealActive = false;
+				}
+			}
+	
+			/* if deal active, add to div */
+			if (dealActive == true) {
+	
+				addToDisplayDeals();
+	
+			}
+		}
+}
