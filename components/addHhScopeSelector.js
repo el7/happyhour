29ccb56fbba30989d78d0d19e2c7776a73fa420a
el7 <got2skatetoday@gmail.com>
2024-12-23 02:52:00 -0800

    refactoring

--- components/addHhScopeSelector.js
+++ components/addHhScopeSelector.js
@@ -5,12 +5,10 @@ export function addHhScopeSelector() {
 	var labelNow = '<label for="radioNow">NOW</label>';
 	var labelHour = '<label for="radioHour">HOUR</label>';
 	var labelToday = '<label for="radioToday">TODAY</label>';
-	var labelOff = '<label for="radioOff">OFF</label>';
 
 	var radioModeNowHTML = '<input type="radio" id="hhModeNowRadio" class="radio hhMode" value="now" name="' + name + '"';
 	var radioModeHourHTML = '<input type="radio" id="hhModeHourRadio" class="radio hhMode" value="hour" name="' + name + '"';
 	var radioModeTodayHTML = '<input type="radio" id="hhModeTodayRadio" class="radio hhMode" value="today" name="' + name + '"';
-	var radioModeOffHTML = '<input type="radio" id="hhModeOffRadio" class="radio hhMode" value="off" name="' + name + '"';
     var radioHtml = "";
 
 	if (checked) {
@@ -20,13 +18,11 @@ export function addHhScopeSelector() {
 	radioModeNowHTML += '/>';
 	radioModeHourHTML += '/>';
 	radioModeTodayHTML += '/>';
-	radioModeOffHTML += '/>';
 
 	radioHtml += 
     radioModeNowHTML + labelNow + 
     radioModeHourHTML + labelHour + 
-    radioModeTodayHTML + labelToday + 
-    radioModeOffHTML + labelOff;
+    radioModeTodayHTML + labelToday;
 
 	var radioFragment = document.createElement('div');
 	radioFragment.innerHTML = radioHtml;
